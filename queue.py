241e161531507239aeb7f7ffa2b36e11b4d8418a
Marc DM <marcdm@fraser>
2015-12-05 21:26:39 -0500

    code. triage and queue function fields

--- queue.py
+++ queue.py
@@ -2,8 +2,9 @@
 from trytond.pool import Pool
 from trytond.model import ModelView, ModelSQL, fields
 from trytond.pyson import Eval, Not, Equal, Or, Greater, In, Len
+from .triage import TriageEntry, SEX_OPTIONS
 
-APPOINTMENT_STATES = [
+QUEUE_ENTRY_STATES = [
     ('0', ''),
     ('1', 'Triage'),
     ('2', 'Registration'),
@@ -14,6 +15,9 @@ APPOINTMENT_STATES = [
 
 
 class QueueEntry(ModelSQL, ModelView):
+    'Queue Entry'
+    __name__ = 'gnuhealth.patient.queue_entry'
+
     active = fields.Boolean('Active')
     triage_entry = fields.Many2One('gnuhealth.triage.entry', 'Triage Entry')
     appointment = fields.Many2One('gnuhealth.appointment', 'Appointment')
@@ -25,9 +29,55 @@ class QueueEntry(ModelSQL, ModelView):
     encounter_component_count = fields.Function(
         fields.Integer('# Components'), 'get_encounter_component_set')
         # searcher='search_component_count')
-    appointment_state = fields.Function(fields.Selection(APPOINTMENT_STATES,
-                                        'State'), 'get_appointment_state')
-                                        # searcher='search_appointment_state')
+    entry_state = fields.Function(fields.Selection(QUEUE_ENTRY_STATES, 'State'),
+                                  'get_qentry_state',
+                                  searcher='search_qentry_state')
+    name = fields.Function(fields.Char('Name'), 'get_patient_name',
+                           searcher='search_patient_name')
+    upi_mrn_id = fields.Function(fields.Char('UPI/MRN/ID#'), 'get_upi_mrn_id',
+                                 searcher='search_upi_mrn_id')
+    sex = fields.Function(fields.Selection(SEX_OPTIONS, 'Sex'),
+                          'get_sex', searcher='search_sex')
+    age = fields.Function(fields.Char('Age'), 'get_age')  #, searcher='search_age')
+    notes = fields.Function(fields.Text('Notes/Info'), 'get_notes_info')
+
+    @classmethod
+    def get_patient_name(cls, instances, name):
+        out = dict([(x.id, x.appointment.patient.name.name) for x in instances
+                    if x.appointment])
+        out.update([(x.id, x.triage_entry.name) for x in instances
+                    if x.triage_entry and not x.appointment])
+        return out
+
+    @classmethod
+    def search_patient_name(cls, name, clause):
+        fld, operator, operand = clause
+        if name == 'name':
+            dom = ['OR']
+            for fld in ('triage_entry.name',
+                        'appointment.patient.name.name'):
+                dom.append((fld, operator, operand))
+            return dom
+
+    @classmethod
+    def get_upi_mrn_id(cls, instances, name):
+        out = dict([(x.id, 'UPI:%s  | MRN: %s' % (
+                        x.appointment.patient.puid,
+                        x.appointment.patient.medical_record_num))
+                    for x in instances if x.appointment])
+
+        out.update([(x.id, x.triage_entry.id_display)
+                    for x in instances if x.triage_entry and not x.appointment])
+
+    @classmethod
+    def search_upi_mrn_id(cls, name, clause):
+        fld, operator, operand = clause
+        out = ['OR'] + [(f, operator, operand) for f in
+                        ('triage_entry.id_number',
+                         'appointment.patient.puid',
+                         'triage_entry.patient.puid',
+                         'triage_entry.patient.medical_record_num')]
+        return out
 
     # get_encounter_component_set = returns a csv string with the most
     # recent component listed first
@@ -50,9 +100,9 @@ class QueueEntry(ModelSQL, ModelView):
     # search_component_count: return a domain to search for queue entries
     # based on the number of components the associated encounter has
 
-    # get_appointment_state: return one from APPOINTMENT_STATES based
+    # get_qentry_state: return one from QUEUE_ENTRY_STATES based
     # on the state of the associated appointment and encounter
-    def get_appointment_state(self, name):
+    def get_qentry_state(self, name):
         if name == 'appointment_state':
             if self.appointment:
                 if self.appointment.state == 'arrived':
@@ -61,12 +111,63 @@ class QueueEntry(ModelSQL, ModelView):
                     return '4'
                 elif self.appointment.state in ['done', 'user_cancelled',
                                                 'center_cancelled', 'no_show']:
-                    return '5'
-            elif self.triage_entry and self.triage_entry in ['tobeseen', 'resched', 'refer']:
+                    return '99'
+            elif self.triage_entry and self.triage_entry.status in [
+                    'tobeseen', 'resched', 'refer']:
                 return '2'
         return '1'
 
-    # search_appointment_state: domain that searches queueEntries based
+    # search_qentry_state: domain that searches queueEntries based
     # the specified state entered.
+    @classmethod
+    def search_qentry_state(cls, name, clause):
+        field, operator, operand = clause
+        if operator == '=':
+            # the easy one and maybe the only one we'll need since
+            # appointment_state is a selection field
+            if operand == '1':
+                return ['OR', ('triage_entry.status', '=', 'pending'),
+                        ('appointment.state', '=', 'confirmed')]
+            elif operand == '2':
+                return [('triage_entry.status', 'in',
+                         ['tobeseen', 'resched', 'refer'])]
+            elif operand == '3':
+                return [('appointment.state', '=', 'arrived')]
+            elif operand == '4':
+                return [('appointment.state', '=', 'processing')]
+            else:
+                return [('appointment.state', 'in', ['done', 'user_cancelled',
+                                                     'center_cancelled',
+                                                     'no_show'])]
 
+    @classmethod
+    def get_sex(cls, instances, name):
+        out = dict([(x.id, x.appointment.patient.age)
+                    for x in instances if x.appointment])
+        out.update([(x.id, x.triage_entry.age)
+                    for x in instances if not x.appointment])
+        return out
 
+    @classmethod
+    def search_sex(cls, name, clause):
+        fld, operator, operand = clause
+        return ['OR', ('triage_entry.sex', operator, operand),
+                ('appointment.patient.name.sex', operator, operand)]
+
+    @classmethod
+    def get_age(cls, instances, name):
+        out = dict([(x.id, x.appointment.patient.age)
+                    for x in instances if x.appointment])
+        out.update([(x.id, x.triage_entry.age)
+                    for x in instances if not x.appointment])
+        return out
+
+    # @classmethod
+    # def search_age(cls, name, clause):
+    #     pass
+
+    def get_notes_info(self, name):
+        if self.appointment:
+            return self.appointment.comments
+        else:
+            return self.triage_entry.notes
--- triage.py
+++ triage.py
@@ -1,7 +1,7 @@
 
 from trytond.pool import Pool
 from trytond.model import ModelView, ModelSQL, fields
-from trytond.pyson import Eval, Not, Equal, Or, Greater, In, Len
+from trytond.pyson import Eval, Not, Equal, Or, Bool, In, Len
 
 SEX_OPTIONS = [('m', 'Male'), ('f', 'Female'), ('u', 'Unknown')]
 ID_TYPES = [
@@ -28,16 +28,21 @@ TRIAGE_STATUS = [
     ('done', 'Done')
 ]
 
+REQD_IF_NOPATIENT = {'required': Not(Bool(Eval('patient'))),
+                     'readonly': Bool(Eval('patient'))}
+
 
 class TriageEntry(ModelSQL, ModelView):
     'Triage Entry'
     __name__ = 'gnuhealth.triage.entry'
-    firstname = fields.Char('First Name')
-    lastname = fields.Char('Last Name')
-    sex = fields.Selection(SEX_OPTIONS, 'Sex')
-    age = fields.Char('Age')
-    id_type = fields.Selection(ID_TYPES, 'ID Type')
+    firstname = fields.Char('First Name', states=REQD_IF_NOPATIENT)
+    lastname = fields.Char('Last Name', states=REQD_IF_NOPATIENT)
+    sex = fields.Selection(SEX_OPTIONS, 'Sex', states=REQD_IF_NOPATIENT)
+    age = fields.Char('Age', states=REQD_IF_NOPATIENT)
+    id_type = fields.Selection(ID_TYPES, 'ID Type', states={
+        'required': Bool(Eval('id_number'))})
     id_number = fields.Char('ID Number')
+    id_display = fields.Function(fields.Char('ID Display'), 'get_id_display')
     patient = fields.Many2One('gnuheath.patient', 'Patient')
     priority = fields.Selection(TRIAGE_PRIO, 'Priority')
     injury = fields.Boolean('Injury')
@@ -45,7 +50,8 @@ class TriageEntry(ModelSQL, ModelView):
     status = fields.Selection(TRIAGE_STATUS, 'Status')
     notes = fields.Text('Notes')
     upi = fields.Function(fields.Char('UPI'), 'get_patient_party_field')
-    name = fields.Function(fields.Char('Name'), 'get_name')
+    name = fields.Function(fields.Char('Name'), 'get_name',
+                           searcher='search_name')
     patient_search = fields.Function(fields.One2Many(
                                      'gnuhealth.patient', None, 'Patients'),
                                      'patient_search_result')
@@ -60,12 +66,31 @@ class TriageEntry(ModelSQL, ModelView):
         return out
 
     @classmethod
+    def search_name(cls, name, clause):
+        fld, operator, operand = clause
+        return ['OR',
+                ('patient.name.name', operator, operand),
+                ('firstname', operator, operand),
+                ('lastname', operator, operand)]
+
+    @classmethod
     def get_patient_party_field(cls, instances, name):
+
         if name == 'name':
             out = dict([(i.id, i.patient.puid) for i in instances
                         if i.patient])
             out.update([(i.id, '') for i in instances if not i.patient])
+        else:
+            out = {}
         return out
+
+    def get_id_display(self, name):
+        idtypedict = dict(ID_TYPES)
+        if self.id_number and self.id_type:
+            return ': '.join([idtypedict.get(x.id_type), x.id_number])
+        else:
+            return ''
+
 
     def patient_search_result(self, name):
         # ToDo: perform search against patient/party and return
