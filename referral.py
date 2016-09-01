daf888d20569105c31cc09fbd873865c90a5bab5
Marc Murray <marcdm@nyzam>
2016-09-01 13:58:16 -0500

    updates and bugfix on referral field and button states

--- referral.py
+++ referral.py
@@ -17,22 +17,25 @@ REFERRAL_SERVICE = [
     ('3', 'Urgent'),
     ('99', 'Routine')
 ]
-RO_HERE = {'readonly': Eval('is_local', True),
-           'invisible': ~(Eval('id', 0) > 0)}
-RO_THERE = {'readonly': ~Eval('is_local', True)}
+RO_HERE = {'readonly': Or(~Eval('is_target', False),
+                          Eval('signed_target', False)),
+           'invisible': ~Eval('signed_local')}
+RO_THERE = {'readonly': Eval('signed_local', False)}
 
 
 class PatientReferral(ModelSQL, ModelView):
     'Referral'
     __name__ = 'gnuhealth.patient.referral'
     name = fields.Many2One('gnuhealth.patient', 'Patient', required=True,
-                           states=RO_THERE)
+                           states={'readonly': Or(Bool(Eval('from_triage')),
+                                                  Eval('signed_local', False),
+                                                  Bool(Eval('from_encounter')))})
     sex = fields.Function(fields.Selection(SEX_OPTIONS, 'Sex'),
                           'get_patient_data',
                           searcher='search_patient_data')
     sex_display = fields.Function(fields.Char('Sex'), 'get_patient_data')
     puid = fields.Function(fields.Char('UPI'), 'get_patient_data',
-                          searcher='search_patient_data')
+                           searcher='search_patient_data')
     age = fields.Function(fields.Char('Current Age'), 'get_patient_data')
     dob = fields.Function(fields.Date('Date of Birth'), 'get_patient_data',
                           searcher='search_patient_data')
@@ -40,11 +43,11 @@ class PatientReferral(ModelSQL, ModelView):
     referral_date = fields.DateTime('Date/Time', required=True,
                                     states=RO_THERE)
     referred_by = fields.Many2One('gnuhealth.healthprofessional', 'Referred by',
-                              states=RO_THERE)
+                                  states=RO_THERE)
     reason = fields.Text('Reason for referral', states=RO_THERE)
     results = fields.Text('Consultant Notes/Findings', states=RO_HERE)
     from_institution = fields.Many2One('gnuhealth.institution', 'Referred From',
-                                  required=True, states=RO_THERE)
+                                       required=True, states=RO_THERE)
     to_institution = fields.Many2One('gnuhealth.institution',
                                      'Refer To', required=True, states=RO_THERE)
     to_specialty = fields.Many2One('gnuhealth.specialty', 'Specialty',
@@ -72,12 +75,16 @@ class PatientReferral(ModelSQL, ModelView):
     referer_sign_date = fields.DateTime('Sign date',
             states={'readonly': True,
                     'invisible': ~Bool(Eval('referer'))})
+    signed_local = fields.Function(fields.Boolean('Referer Signed'),
+                                   'get_signed', searcher='search_signed')
     referee = fields.Many2One('gnuhealth.healthprofessional', 'Referee',
             states={'readonly': True,
                     'invisible': ~Bool(Eval('referee_sign_date'))})
     referee_sign_date = fields.DateTime('Referee Sign Date',
             states={'readonly': True,
                     'invisible': ~Bool(Eval('referee'))})
+    signed_target = fields.Function(fields.Boolean('Referee Signed'),
+                                   'get_signed', searcher='search_signed')
 
     @classmethod
     def __setup__(cls):
@@ -87,9 +94,15 @@ class PatientReferral(ModelSQL, ModelView):
             'associated with this user'
         })
         cls._buttons.update({
-            'btn_sign': {'readonly': Or(~Bool(Eval('name', False)),
-                                        And(Bool(Eval('referer_sign_date')),Eval('is_local', False)),
-                                        And(Eval('is_target', False), Bool(Eval('referee_sign_date'))))}
+            'btn_sign_local': {
+                'invisible': Eval('signed_local', False),
+                'readonly': ~Bool(Eval('name', False))
+            },
+            'btn_sign_target': {
+                'invisible': Or(Eval('signed_target', False),
+                                ~Eval('signed_local', False),
+                                ~Eval('is_target', False))
+            }
         })
 
     def get_patient_data(self, name):
@@ -129,7 +142,7 @@ class PatientReferral(ModelSQL, ModelView):
         return dict(local_list)
 
     @classmethod
-    def get_target_here(cls, instances, name):
+    def get_is_target(cls, instances, name):
         HI = Pool().get('gnuhealth.institution')
         hi = HI.get_institution()
         target_list = [(i.id, int(i.to_institution) == hi)
@@ -160,8 +173,7 @@ class PatientReferral(ModelSQL, ModelView):
             return [(field_name, '!=', hi)]
 
     @classmethod
-    @ModelView.button
-    def btn_sign(cls, referrals):
+    def do_sign(cls, referrals, sign_local=True):
         HP = Pool().get('gnuhealth.healthprofessional')
         signing_hp = HP.get_health_professional()
         if not signing_hp:
@@ -170,9 +182,9 @@ class PatientReferral(ModelSQL, ModelView):
         target_sign = []
         now = datetime.now()
         for referral in referrals:
-            if referral.is_local and referral.name:
+            if sign_local:
                 local_sign.append(referral)
-            elif referral.is_target:
+            else:
                 target_sign.append(referral)
         if local_sign:
             local_write = {'referer': signing_hp, 'referer_sign_date': now}
@@ -180,4 +192,48 @@ class PatientReferral(ModelSQL, ModelView):
         if target_sign:
             target_write = {'referee': signing_hp, 'referee_sign_date': now}
             cls.write(target_sign, target_write)
+
+    @classmethod
+    @ModelView.button
+    def btn_sign_local(cls, referrals):
+        cls.do_sign(referrals, True)
+
+    @classmethod
+    @ModelView.button
+    def btn_sign_target(cls, referrals):
+        cls.do_sign(referrals, False)
+
+    @classmethod
+    def get_signed(cls, instances, name):
+        if name == 'signed_local':
+            field_name = 'referer_sign_date'
+        else:
+            field_name = 'referee_sign_date'
+        outlist = [(int(i), getattr(i, field_name) is not None)
+                   for i in instances]
+        print ('%s\n\nfield_name= %s outlist = %s' % ('*'*70, name, repr(outlist)))
+        return dict(outlist)
+
+    @classmethod
+    def search_signed(cls, name, clause):
+        fld, operator, operand = clause
+        operdict = {'=': '!=', '!=': '='}
+        if name == 'signed_local':
+            field_name = 'referer_sign_date'
+        else:
+            field_name = 'referee_sign_date'
+        if operator in operdict:
+            if operand is True:
+                return [(field_name, operdict[operator], None)]
+            else:
+                return [(field_name, operator, None)]
+
+    @staticmethod
+    def default_signed_local():
+        return False
+
+    @staticmethod
+    def default_signed_target():
+        return False
+
 
